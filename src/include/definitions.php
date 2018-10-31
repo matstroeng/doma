525fc17a51a562acaa4f21112f3d8657473bd9a8
Mats Troeng <mats@matstroeng.se>
2018-10-31 22:57:02 +0100

    Setting version number to 3.0.7.

--- src/include/definitions.php
+++ src/include/definitions.php
@@ -1,5 +1,5 @@
 <?php
-  define('DOMA_VERSION', '3.0.6');
+  define('DOMA_VERSION', '3.0.7');
   define('DOMA_SERVER', 'http://www.matstroeng.se/doma/domaserver.php');
 
   $rootPath =  dirname(dirname(__FILE__));
