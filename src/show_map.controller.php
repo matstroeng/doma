e6be7a7043494be962bbe5e82538af309e551a2e
John Kasper Svergja <john.k.svergja@gmail.com>
2023-03-12 15:24:56 +0100

    Support '&amp;' in url parameter for show_map.php
    Solves  #28

--- src/show_map.controller.php
+++ src/show_map.controller.php
@@ -16,7 +16,9 @@
 
       // the requested map
       $map = new Map();
-      $map->Load($_GET["map"]);
+      //Solves https://github.com/matstroeng/doma/issues/28
+      $map_id = $_GET["map"] ?: $_GET["amp;map"];
+      $map->Load($map_id);
       
       if(!$map->ID) die("The map has been removed.");
       
